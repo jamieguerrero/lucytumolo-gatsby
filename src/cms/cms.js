5999e336d8eb561b73bac59fa3dca239b0ff4424
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2018-07-27 18:23:34 -0400

    Create footer and home fields

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,6 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
 import FAQPagePreview from './preview-templates/FAQPagePreview'
 import GiftPagePreview from './preview-templates/GiftPagePreview'
@@ -12,7 +11,6 @@ import TestimonialPreview from './preview-templates/TestimonialPreview'
 import ResourcesPreview from './preview-templates/ResourcesPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('faq', FAQPagePreview)
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid'
+import Content, { HTMLContent } from '../components/Content'
 
 const Testimonials = ({
   testimonials
 }) => {
+
   return (
   <div>
     {testimonials.map(testimonial => {
-      console.log(testimonial)
       const { testimonialImage } = testimonial.frontmatter
       return (
         <article key={v4()} className="message">
           <div className="message-body">
             <img src={testimonialImage}/>
+            <HTMLContent content={testimonial.html} />
           </div>
         </article>)
     })}
@@ -24,7 +26,7 @@ Testimonials.propTypes = {
   testimonials: PropTypes.arrayOf(
     PropTypes.shape({
       testimonialImage: PropTypes.string,
-      testimonial: PropTypes.string,
+      title: PropTypes.string,
     })
   ),
 }
