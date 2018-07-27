20268b529ce4faa5688555a0c866b5dab03a299f
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2018-07-26 23:44:56 -0400

    resources-page

--- src/pages/testimonials/test-1.md
+++
@@ -1,6 +0,0 @@
----
-templateKey: testimonial-post
-title: Test 1
-testimonialImage: /img/about-lucy.jpg
-testimonial: Test testimonial 1
----
---
+++ src/templates/resources-page.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const ModalitiesPageTemplate = ({
+  title,
+  heroImage,
+  modalities
+}) => (
+  <section className="section section--gradient">
+    <div className="container">
+      {title}
+      <img src={heroImage}/>
+      {modalities}
+    </div>
+  </section>
+)
+
+ModalitiesPageTemplate.propTypes = {
+  title: PropTypes.string,
+  heroImage: PropTypes.string,
+  modalities: PropTypes.string
+}
+
+const ModalitiesPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <ModalitiesPageTemplate
+      title={frontmatter.title}
+      heroImage={frontmatter.heroImage}
+      modalities={frontmatter.modalities}
+    />
+  )
+}
+
+ModalitiesPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default ModalitiesPage
+
+export const modalitiesPageQuery = graphql`
+  query ModalitiesPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        heroImage
+        modalities
+      }
+    }
+  }
+`
