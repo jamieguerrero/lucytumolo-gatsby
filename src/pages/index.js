427559cb33485721cbc01ad9ecc1b36c874784fe
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2018-07-27 20:31:25 -0400

    graphql query on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link' 
+import Link from 'gatsby-link'
 
 import hero from '../img/hero-background.jpg'
 
@@ -8,38 +8,11 @@ export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
-
+    console.log(data)
     return (
       <section className="section">
         <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
-          </div>
 
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
-            ))}
         </div>
       </section>
     )
@@ -56,21 +29,37 @@ IndexPage.propTypes = {
 
 export const pageQuery = graphql`
   query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-    ) {
+    allMarkdownRemark(filter: {frontmatter: {templateKey: {regex: "/home-page/"}}}) {
       edges {
         node {
-          excerpt(pruneLength: 400)
           id
-          fields {
-            slug
-          }
-          frontmatter {
+          frontmatter{
             title
             templateKey
-            date(formatString: "MMMM DD, YYYY")
+            ossingtonLink
+            dundasLink
+            servicesTitle
+            servicesImage
+            servicesDescription
+            modality1 {
+              description
+              image
+              title
+            }
+            modality2 {
+              description
+              image
+              title
+            }
+            modality3 {
+              description
+              image
+              title
+            }
+            parallaxImage
+            ratesTitle
+            ratesImage
+            ratesDescription
           }
         }
       }
