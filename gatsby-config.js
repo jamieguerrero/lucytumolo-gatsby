5516c6778cd2a3c69b6b39e0217f44aaf95a517b
Jamie Guerrero <jamie.n.guerrero@gmail.com>
2018-10-15 20:12:12 -0400

    added enableIdentityWidget: true to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
+        enableIdentityWidget: true,
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
